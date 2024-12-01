8ff6073c3d1f4597bc1b6ad4c26b7d052caa4d6f
Aaron Evans <aarone@outlook.com.au>
2024-12-02 09:10:42 +1100

    clean up

--- tenant-config-project/providers.tf
+++ tenant-config-project/providers.tf
@@ -1,20 +1,2 @@
-provider "aws" {
-  region = var.region
-}
-
 provider "tfe" {
-  hostname = var.tfc_hostname
-}
-
-terraform {
-  cloud {
-    hostname     = "tfe.simon-lynch.sbx.hashidemos.io"
-    organization = "myorg"
-
-    workspaces {
-      name    = "tfc-tenant-config"
-      project = "platform_team"
-    }
-  }
-
 }
